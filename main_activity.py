4ca0c7e0ad972cad562027e86263d4e729c8663b
tjl-bautista <btl1223@dlsud.edu.ph>
2023-04-15 11:48:23 +0800

    cleaned and optimized code

--- main_activity.py
+++ main_activity.py
@@ -1,46 +1,44 @@
-'''
-5 Enabling Assessment, three (3) Summative Assessment, Final Examination
-'''
-# Grade  = (ea_avg * 0.3) + (sa_avg * 0.3) + (final_exam * 0.4)
+def calculate_grade (ea_avg, sa_avg, final_exam):
+    return (ea_avg * 0.3) + (sa_avg * 0.3) + (final_exam * 0.4)
 
+def calculate_letter (final_grade):
+    if final_grade[i] >= 90:
+        return 'A'
+    elif final_grade[i] >= 80:
+        return 'B'
+    elif final_grade[i] >= 70:
+        return 'C'
+    elif final_grade[i] >= 60:
+        return 'D'
+    else:
+        return 'F'
+    
 student_name = []
 final_grade = []
 letter_grade = []
 
-for i in range(5):
-    print("\n")
-    print(f"[[Student {i+1}]] ")
+for i in range(2):
+    print(f"\n[[Student {i+1}]] ")
     student_name.append(input("Enter the Student Name: "))
 
     # 5 Enabling Assessments
-    enabling_asssessment = []
+    ea = []
     for x in range(5):    
-        enabling_asssessment.append(float(input(f"Enter Enabling Assessment {x+1} grade: ")))
-    ea_avg = sum(enabling_asssessment) / len(enabling_asssessment)
-
+        ea.append(float(input(f"Enter Enabling Assessment {x+1} grade: ")))
+    ea_avg = sum(ea) / len(ea)
+    
     # 3 Summative Assessments
-    summative_asssessment = []
+    sa = []
     for y in range(3):    
-        summative_asssessment.append(float(input(f"Enter Summatieve Assessment {y+1} grade: ")))
-    sa_avg = sum(summative_asssessment) / len(summative_asssessment)
+        sa.append(float(input(f"Enter Summative Assessment {y+1} grade: ")))
+    sa_avg = sum(sa) / len(sa)
     
     # Final Examination
     final_exam = float(input("Enter Final Examination grade: "))
 
-    # Final Grade
-    final_grade.append((ea_avg * 0.3) + (sa_avg * 0.3) + (final_exam * 0.4))
-
-    # Letter Grade 
-    if final_grade[i] >= 90:
-        letter_grade.append('A')
-    elif final_grade[i] >= 80:
-        letter_grade.append('B')
-    elif final_grade[i] >= 70:
-        letter_grade.append('C')
-    elif final_grade[i] >= 60:
-        letter_grade.append('D')
-    else:
-        letter_grade.append('F')
+    # Final Grade and Letter Grade
+    final_grade.append(calculate_grade(ea_avg, sa_avg, final_exam))
+    letter_grade.append(calculate_letter(final_grade))
 
 # Table output (not the cleanest code :<)
 print("\n\n\n\n")
